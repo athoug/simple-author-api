f52865bfac15b0aadc08742dc0b9651e168ee5cc
Athoug <athoug.s@gmail.com>
2024-06-27 13:02:26 +0300

    adding ui for authors

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const replaceTemplate = require('./models/replaceTemplate');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
 // read the file
 const data = fs.readFileSync(`${__dirname}/data/authors.json`, 'utf-8');
 const authors = JSON.parse(data);
+const index = fs.readFileSync(`${__dirname}/templates/index.html`, 'utf-8');
+const card = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
 	const { query, pathname } = url.parse(req.url, true);
@@ -21,7 +25,11 @@ const server = http.createServer((req, res) => {
 		res.writeHead(200, {
 			'Content-Type': 'text/html',
 		});
-		res.end('<h1>Hello Authors</h1>');
+
+		const cardsHtml = authors.map((el) => replaceTemplate(card, el)).join('');
+		const output = index.replace(/{%AUTHOR_CARDS%}/g, cardsHtml);
+
+		res.end(output);
 	} else if (pathname === '/author') {
 		const author = authors.find(
 			(author) =>
---
+++ models/replaceTemplate.js
@@ -0,0 +1,19 @@
+// ----- helper methods -----
+const replaceTemplate = (temp, author) => {
+	let output = temp.replace(/{%PHOTOS%}/g, author.attributes.photos);
+	output = output.replace(/{%AUTHOR_NAME%}/g, author.attributes.name);
+	output = output.replace(/{%NATIONALITY%}/g, author.attributes.nationality);
+	output = output.replace(/{%WIKIPEDIA%}/g, author.attributes.wikipedia);
+	output = output.replace(/{%ID%}/g, author.id);
+	output = output.replace(/{%BLURB%}/g, author.attributes.blurb);
+
+	if (!author.organic) {
+		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+	} else {
+		output = output.replace(/{%NOT_ORGANIC%}/g, '');
+	}
+
+	return output;
+};
+
+module.exports = replaceTemplate;
