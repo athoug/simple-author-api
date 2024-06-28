586a6993615b2eebe937fb3dce3d4c2006009f1f
Athoug <athoug.s@gmail.com>
2024-06-28 20:36:34 +0300

    adding flags

--- models/replaceTemplate.js
+++ models/replaceTemplate.js
@@ -3,6 +3,7 @@ const replaceTemplate = (temp, author) => {
 	let output = temp.replace(/{%PHOTOS%}/g, author.attributes.photos);
 	output = output.replace(/{%AUTHOR_NAME%}/g, author.attributes.name);
 	output = output.replace(/{%NATIONALITY%}/g, author.attributes.nationality);
+	output = output.replace(/{%FLAG%}/g, author.attributes.flag);
 	output = output.replace(/{%WIKIPEDIA%}/g, author.attributes.wikipedia);
 	output = output.replace(/{%ID%}/g, author.id);
 	output = output.replace(/{%BLURB%}/g, author.attributes.blurb);
