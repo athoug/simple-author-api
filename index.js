61b6f1dab8a8eed39f77672403bcd5b08c2c33b5
Athoug <athoug.s@gmail.com>
2024-06-28 19:20:26 +0300

    adding fav icon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const path = require('path');
 
 const replaceTemplate = require('./models/replaceTemplate');
 
@@ -13,9 +14,14 @@ const authors = JSON.parse(data);
 const index = fs.readFileSync(`${__dirname}/templates/index.html`, 'utf-8');
 const card = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 
+// create the server
 const server = http.createServer((req, res) => {
+	// get the url and query parameters
 	const { query, pathname } = url.parse(req.url, true);
 
+	console.log(pathname);
+
+	// setup the routes
 	if (pathname === '/api') {
 		res.writeHead(200, {
 			'Content-Type': 'application/json',
@@ -47,6 +53,16 @@ const server = http.createServer((req, res) => {
 			});
 			res.end('<h1>Author Not Found</h1>');
 		}
+	} else if (pathname === '/books.png') {
+		fs.readFile(path.join(__dirname, 'public', 'books.png'), (err, data) => {
+			if (err) {
+				res.writeHead(500, { 'Content-Type': 'text/plain' });
+				res.end('500 Internal Server Error');
+				return;
+			}
+			res.writeHead(200, { 'Content-Type': 'image/png' });
+			res.end(data);
+		});
 	} else {
 		res.writeHead(404, {
 			'Content-Type': 'text/html',
@@ -56,5 +72,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-	console.log(`listenint on http://${hostname}:${port}`);
+	console.log(`listening on http://${hostname}:${port}`);
 });
